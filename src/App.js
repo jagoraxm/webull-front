22c935c8ef66bf28b870fcbf5e4221ac677d3f9e
jagoraxr <jagoraxr.moucha@gmail.com>
2024-09-12 02:22:39 -0600

    hotfix: cors allow origin

--- src/App.js
+++ src/App.js
@@ -16,7 +16,10 @@ function App() {
       url: baseURL,
       headers: { 
         'Authorization': `Bearer ${token}`,
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
+        'origin':'x-requested-with',
+        'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
+        'Content-Type': 'application/json',
       },
       //data : data
     };
